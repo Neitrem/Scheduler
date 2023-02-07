c7faa4ca2f6ef6384d1947092cafd70c19517e57
Neitrem <a.v.gorshenin000@gmail.com>
2023-02-07 19:35:44 +0300

    v0.3.5

--- src/components/MyCharacter.vue
+++ src/components/MyCharacter.vue
@@ -4,8 +4,14 @@
             <div class="link inline clickable">
                 <input v-model="character.link" type="text" placeholder="Ссылка на человека">
             </div>
-            <button class="settings inline clickable"><i class="bi bi-three-dots-vertical"></i></button>
-            <i @click="deleteThis" class="bi bi-trash inline delete"></i>            
+            <button class="settings inline clickable">
+                <svg style="height: 30px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                </svg>
+            </button>
+            <svg @click="deleteThis" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3 inline delete" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+            </svg>
             <div class="weight inline clickable">
                 <select class="select" v-model="character.weight">
                     <option  v-for="option in weightList.options" v-bind:value="option" v-bind:key="option">
@@ -119,6 +125,8 @@ export default {
         }
         .delete {
             
+            margin: auto 0;
+
             &:hover {
                 color: brown;
             }
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: path.resolve(__dirname, "./dist/"),
   plugins: [vue()],
+  base: path.resolve(__dirname, "./dist/"),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-  
+  },
 })
